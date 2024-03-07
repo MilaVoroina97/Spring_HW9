b0c2ff6f20df042bafcd398a83d92ecd89e640b5
mila <milavoronina1997@mail.ru>
2024-03-07 20:09:06 +0500

    test: correct application configuration after testing

---
+++ apigateway/src/main/java/com/voronina/gb/apigateway/RecordGateway.java
@@ -0,0 +1,24 @@
+package com.voronina.gb.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class RecordGateway {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecordGateway.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("Microservice1",r->r.path("/records/**")
+                        .uri("http://localhost:8080/"))
+                .route("Microservice2",r->r.path("/users/**")
+                        .uri("http://localhost:8081/")).build();
+    }
+}
