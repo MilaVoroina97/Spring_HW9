ee7b7aede89bf0ca51a0a2f599d1547ee185239e
mila <milavoronina1997@mail.ru>
2024-03-07 17:09:26 +0500

    first commit

---
+++ records/src/main/java/com/voronina/gb/Record.java
@@ -0,0 +1,10 @@
+package com.voronina.gb;
+
+public class Record {
+
+    private int id;
+    private String reportingPeriod;
+    private String projectSection;
+    private double price;
+    private String status;
+}
